d04d67ae753593743fda591cc9f6edbcf6bbf605
abishekaditya <abishekaditya@outlook.com>
2015-04-21 22:48:41 +0530

    Revert "deleted accidentaly"
    This reverts commit b53483d748b2d9eb387b1064f27a4b3cbf80555e.

---
+++ decompressor.h
@@ -0,0 +1,18 @@
+//=================================
+// include guard
+#ifndef DECOMPRESSOR_H
+#define DECOMPRESSOR_H
+
+//=================================
+// forward declared dependencies
+
+
+//=================================
+// included dependencies
+
+
+//=================================
+// the actual class
+
+
+#endif
