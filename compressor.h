d64715630ae64ea006e2a3aea7d762525c67bdda
Saurabh Mathur <saurabhmathur96@gmail.com>
2015-04-27 20:03:05 +0530

    Update compressor.h

--- compressor.h
+++ compressor.h
@@ -1,18 +1,41 @@
-//=================================
-// include guard
 #ifndef COMPRESSOR_H
 #define COMPRESSOR_H
 
-//=================================
-// forward declared dependencies
+#include <cstdint>
+#include <cstdlib>
+#include <exception>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <istream>
+#include <limits>
+#include <map>
+#include <ostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "ConfigUtils.h"
+
+using namespace std;
+
+class Compressor
+{
+  
+  public:
+    Compressor(string input_file, string output_file);
+    void compress();
+    ~Compressor();
+    
+  private:
+    
+    map<vector<char>, CodeType> dictionary;
+    ofstream output_stream;
+    ifstream input_stream;
+    void reset_dictionary();
+    
+};
 
 
-//=================================
-// included dependencies
 
-
-//=================================
-// the actual class
-
-
-#endif 
+#endif
