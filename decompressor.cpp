c5085d082c580c75eb6d02a91300d95c9d42d095
abishekaditya <abishekaditya@outlook.com>
2015-04-28 11:32:30 +0530

    Updated
    vector<vector<char>> behaving like a madman

--- decompressor.cpp
+++ decompressor.cpp
@@ -1,1 +1,62 @@
 #include "decompressor.h"
+
+using namespace std;
+
+Decompressor::Decompressor(string input_file, string output_file)
+{
+	 input_stream.open(input_file, ios_base::binary);
+  output_stream.open(output_file, ios_base::binary);
+}
+
+Decompressor::~Decompressor()
+{
+	input_stream.close();
+	output_stream.close();
+}
+
+void Decompressor :: reset_dictionary()
+{
+	const long int minc = numeric_limits<char>::min();
+    const long int maxc = numeric_limits<char>::max();
+
+	dictionary.clear();
+
+
+    dictionary.reserve(globals::dms);
+        for (long int c = minc; c <= maxc; ++c)
+            {
+            	dictionary.push_back({static_cast<char> (c)});
+            }
+}
+
+void Decompressor::decompress()
+{
+    reset_dictionary();
+
+    vector<vector<char>> dictionary;
+    vector<char> s;
+    CodeType k;
+
+    while (is.read(reinterpret_cast<char *> (&k), sizeof (CodeType)))
+    {
+
+        if (dictionary.size() == globals::dms)
+            reset_dictionary();
+
+        if (k > dictionary.size())
+            throw runtime_error("invalid compressed code");
+
+        if (k == dictionary.size())
+            dictionary.push_back(s + s.front());
+        else
+        if (!s.empty())
+            dictionary.push_back(s + dictionary.at(k).front());
+
+        os.write(&dictionary.at(k).front(), dictionary.at(k).size());
+        s = dictionary.at(k);
+    }
+
+    if (!is.eof() || is.gcount() != 0)
+        throw runtime_error("corrupted compressed file");
+}
+}
--- decompressor.h
+++ decompressor.h
@@ -1,18 +1,41 @@
-//=================================
-// include guard
 #ifndef DECOMPRESSOR_H
 #define DECOMPRESSOR_H
 
-//=================================
-// forward declared dependencies
+#include <cstdint>
+#include <cstdlib>
+#include <exception>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <istream>
+#include <limits>
+#include <map>
+#include <ostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
+#include "ConfigUtils.h"
 
-//=================================
-// included dependencies
+using namespace std;
 
+class Decompressor
+{
 
-//=================================
-// the actual class
+  private:
+
+    void reset_dictionary(vector<vector<char>> &dictionary);
+    vector<vector<char>>, CodeType> dictionary;
+    ofstream output_stream;
+    ifstream input_stream;
+
+  public:
+
+    Decompressor(string input_file,string output_file);
+    void decompress();
+    ~Decompressor();
+
+};
 
 
 #endif
